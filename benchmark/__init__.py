7d5b2d1e075e995cda794f533ff814fdc48c6308
kashima19960 <2494389920@qq.com>
2025-06-14 14:38:58 +0800

    添加基准测试模块的初始化文件，包含测试函数和基准问题的导入

---
+++ benchmark/__init__.py
@@ -0,0 +1,7 @@
+"""
+基准测试模块
+"""
+
+from .test_functions import TestFunctions, BenchmarkProblem, get_benchmark_problems, get_simple_problems
+
+__all__ = ['TestFunctions', 'BenchmarkProblem', 'get_benchmark_problems', 'get_simple_problems']
